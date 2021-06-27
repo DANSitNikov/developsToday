cca1fdd331ca1f61e70cfc256b7ed3d5a552325f
Daniil <sitnikovdan13@gmail.com>
2021-06-27 23:31:42 +0300

    feat: add babel

--- test/pages/_document.tsx
+++ test/pages/_document.tsx
@@ -1,32 +1,56 @@
 import React from 'react';
-import Document, { DocumentContext } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { ServerStyleSheets } from '@material-ui/styles';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     static async getInitialProps(ctx: DocumentContext) {
-        const sheet = new ServerStyleSheet();
+        const styledComponentsSheet = new ServerStyleSheet();
+        const materialSheets = new ServerStyleSheets();
         const originalRenderPage = ctx.renderPage;
 
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
-                    // eslint-disable-next-line react/jsx-props-no-spreading
-                    enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+                    enhanceApp: (App) => (props) =>
+                        styledComponentsSheet.collectStyles(materialSheets.collect(<App {...props} />)),
                 });
-
             const initialProps = await Document.getInitialProps(ctx);
             return {
                 ...initialProps,
                 styles: (
-                    <>
+                    <React.Fragment>
                         {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
+                        {materialSheets.getStyleElement()}
+                        {styledComponentsSheet.getStyleElement()}
+                    </React.Fragment>
                 ),
             };
         } finally {
-            sheet.seal();
+            styledComponentsSheet.seal();
         }
     }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    render() {
+        return (
+            <Html lang="en" dir="ltr">
+                <Head>
+                    <meta charSet="utf-8" />
+                    {/* PWA primary color */}
+                    <link
+                        rel="stylesheet"
+                        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
